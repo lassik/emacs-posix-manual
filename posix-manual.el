d1685338bfcc0bdf7d8b9951b79d6fc7824ed07b
Lassi Kortela <lassi@lassi.io>
2023-12-15 17:41:18 +0200

    Turn blank comment lines into blank lines

--- posix-manual.el
+++ posix-manual.el
@@ -1,5 +1,5 @@
 ;;; posix-manual.el --- POSIX manual page lookup -*- lexical-binding: t -*-
-;;
+
 ;; Copyright 2019 Lassi Kortela
 ;; SPDX-License-Identifier: ISC
 ;; Author: Lassi Kortela <lassi@lassi.io>
@@ -7,14 +7,14 @@
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24"))
 ;; Keywords: languages util
-;;
+
 ;; This file is not part of GNU Emacs.
-;;
+
 ;;; Commentary:
-;;
+
 ;; Provides a `posix-manual-entry` command with tab completion. It
 ;; lets you easily call up POSIX manual pages in your web browser.
-;;
+
 ;;; Code:
 
 (require 'browse-url)
